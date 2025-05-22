30cb267d42c6a45e24fbc6c76f52f199dcc6e4b8
azaLiza <liza_boumali@hotmail.com>
2025-05-22 11:44:40 +0200

    add helloworld "useless"

---
+++ helloworld/src/main/java/com/openclassrooms/helloworld/model/HelloWorld.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.openclassrooms.helloworld.model;
+
+/**
+ * 
+ */
+public class HelloWorld {
+	
+	private String value = "Hi world";
+	
+	
+	
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+	
+	
+	
+
+}
---
+++ helloworld/src/test/java/com/openclassrooms/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,32 @@
+package com.openclassrooms.helloworld;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.openclassrooms.helloworld.service.BusinessService;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+	
+	@Autowired
+	private BusinessService bs;
+	
+	
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void testGetHelloWorld() {
+		
+		String expected = "Hi world";
+		String result = bs.getHelloWorld().getValue();
+		
+		assertEquals(expected , result);
+	}
+
+}
